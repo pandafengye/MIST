4ea4fee3c4f329cf1d80e1e4842c3534e2d6c983
pandafengye <58614528+pandafengye@users.noreply.github.com>
2021-06-28 22:43:39 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-python3 MIST.py species -p 8 -1 Example_Dir/input/read/test.1.fq -2 Example_Dir/input/read/test.2.fq -d Pre-built-pangenome/ -o Example_Dir/output/
-python3 MIST.py cluster -t 8 -i  Example_Dir/input/ref_dir/ -s 0.98,0.99,0.999  -o Example_Dir/output/
-python3 MIST.py index -i Example_Dir/input/ref_dir/ -o Example_Dir/output/
-python3 MIST.py map -p 8 -i Example_Dir/output/_MIST_index/ -1 Example_Dir/input/read/test.1.fq -2 Example_Dir/input/read/test.2.fq -l 200 -o Example_Dir/output/
-python3 MIST.py subspecies -c Example_Dir/output/_MIST_ref_cluster.csv -m Example_Dir/output/_MIST_map_Mismatch_matrix.csv -l 200 -o Example_Dir/output/
